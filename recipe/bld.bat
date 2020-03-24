3ac71db9487875d87ef8ccc1051953940613738a
Ryan Volz <rvolz@mit.edu>
2020-03-24 16:22:59 -0400

    Need to set PKG_CONFIG_PATH as env variable.
    Setting it as a meson option works for meson, but g-ir-scanner also uses
    pkg-config during the build and it will fail to find .pc files if the
    environment variable isn't set. Doing so takes care of both use cases,
    so that's preferable. It doesn't seem like the failure to find the .pc
    files with g-ir-scanner breaks the build, but it's best to clear up the
    error anyway.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,14 @@
 setlocal EnableDelayedExpansion
 @echo on
 
+:: set pkg-config path so that host deps can be found
+:: (set as env var so it's used by both meson and during build with g-ir-scanner)
+set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig"
+
 :: meson options
 :: (set pkg_config_path so deps in host env can be found)
 set ^"MESON_OPTIONS=^
   --prefix="%LIBRARY_PREFIX%" ^
-  --pkg-config-path="%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig" ^
   --wrap-mode=nofallback ^
   --buildtype=release ^
   --backend=ninja ^
