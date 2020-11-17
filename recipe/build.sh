6b3ab5322e2f7bdc801044bf86e4ea7494d49b97
Isuru Fernando <isuruf@gmail.com>
2020-11-17 22:29:35 +0000

    use introspection outputs from native build

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,37 @@ if [[ "$target_platform" == osx-* ]]; then
   export LIBS="$LIBS -framework CoreFoundation -framework ApplicationServices"
 fi
 
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
+  (
+    mkdir -p build-host
+    pushd build-host
+
+    export CC=$CC_FOR_BUILD
+    export AR=($CC_FOR_BUILD -print-prog-name=ar)
+    export NM=($CC_FOR_BUILD -print-prog-name=nm)
+    export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
+    export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
+
+    # Unset them as we're ok with builds that are either slow or non-portable
+    unset CFLAGS
+    unset CPPFLAGS
+    export host_alias=$build_alias
+
+    ./configure --prefix=$BUILD_PREFIX \
+                --with-xft \
+                --with-cairo=$BUILD_PREFIX \
+
+    # This script would generate the functions.txt and dump.xml and save them
+    # This is loaded in the native build. We assume that the functions exported
+    # by glib are the same for the native and cross builds
+    export GI_CROSS_LAUNCHER=$PREFIX/libexec/gi-cross-launcher-save.sh
+    make -j${CPU_COUNT}
+    make install
+    popd
+  )
+  export GI_CROSS_LAUNCHER=$PREFIX/libexec/gi-cross-launcher-load.sh
+fi
+
 # Cf. https://github.com/conda-forge/staged-recipes/issues/673, we're in the
 # process of excising Libtool files from our packages. Existing ones can break
 # the build while this happens.
