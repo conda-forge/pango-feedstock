59ce2fbc1bb8faffd8fc8a40fcd175804c2b753d
Isuru Fernando <isuruf@gmail.com>
2020-11-18 10:05:25 -0600

    Remove debug and fix g-ir-compiler

--- recipe/build.sh
+++ recipe/build.sh
@@ -32,8 +32,6 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
                 --with-xft \
                 --with-cairo=$BUILD_PREFIX \
 
-    file $PREFIX/lib/gobject-introspection/giscanner/_giscanner.cpython-39-darwin.so
-    file $BUILD_PREFIX/lib/gobject-introspection/giscanner/_giscanner.cpython-39-darwin.so
     # This script would generate the functions.txt and dump.xml and save them
     # This is loaded in the native build. We assume that the functions exported
     # by glib are the same for the native and cross builds
@@ -41,12 +39,9 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
     make -j${CPU_COUNT}
     make install
     cp $BUILD_PREFIX/bin/g-ir-scanner $PREFIX/bin/g-ir-scanner
-    file $PREFIX/lib/gobject-introspection/giscanner/_giscanner.cpython-39-darwin.so
-    file $BUILD_PREFIX/lib/gobject-introspection/giscanner/_giscanner.cpython-39-darwin.so
+    cp $BUILD_PREFIX/bin/g-ir-compiler $PREFIX/bin/g-ir-compiler
     rsync -ahvpiI $BUILD_PREFIX/lib/gobject-introspection/ $PREFIX/lib/gobject-introspection/
     popd
-    file $PREFIX/lib/gobject-introspection/giscanner/_giscanner.cpython-39-darwin.so
-    file $BUILD_PREFIX/lib/gobject-introspection/giscanner/_giscanner.cpython-39-darwin.so
   )
   export GI_CROSS_LAUNCHER=$PREFIX/libexec/gi-cross-launcher-load.sh
 fi
