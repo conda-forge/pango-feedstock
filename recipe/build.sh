f08b5dc8c094ce39c4e7901be1bff388e8c54f7f
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2021-03-22 20:08:45 +0000

    Bump to 1.48.3

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-set -x
+set -xeo pipefail
 
-unset _CONDA_PYTHON_SYSCONFIGDATA_NAME
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
-
-if [[ "$target_platform" == osx-* ]]; then
-  # For some reason, these are not getting added in osx-arm64 platform.
-  # since it doesn't hurt, add these to all osx-* platforms.
-  export LIBS="$LIBS -framework CoreFoundation -framework ApplicationServices"
+if [[ "$target_platform" = osx-* ]] ; then
+    # The -dead_strip_dylibs option breaks g-ir-scanner in this package: the
+    # scanner links a test executable to find paths to dylibs, but with this
+    # option the linker strips them out. The resulting error message is
+    # "ERROR: can't resolve libraries to shared libraries: ...".
+    export LDFLAGS="$(echo $LDFLAGS |sed -e "s/-Wl,-dead_strip_dylibs//g")"
+    export LDFLAGS_LD="$(echo $LDFLAGS_LD |sed -e "s/-dead_strip_dylibs//g")"
 fi
 
+meson_options_common=(
+    --buildtype=release
+    --backend=ninja
+    -Dlibdir=lib
+    -Dintrospection=enabled
+    -Duse_fontconfig=true
+    -Dgtk_doc=false
+)
+meson_options_build=("${meson_options_common[@]}")
+meson_options_host=("${meson_options_common[@]}")
+
+
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
+  unset _CONDA_PYTHON_SYSCONFIGDATA_NAME
   (
     mkdir -p native-build
     pushd native-build
@@ -26,59 +38,34 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
     # Unset them as we're ok with builds that are either slow or non-portable
     unset CFLAGS
     unset CPPFLAGS
-    export host_alias=$build_alias
 
-    ../configure --prefix=$BUILD_PREFIX \
-                --with-xft \
-                --with-cairo=$BUILD_PREFIX \
-
+    meson "${meson_options_build[@]}" --prefix=$BUILD_PREFIX ..
     # This script would generate the functions.txt and dump.xml and save them
     # This is loaded in the native build. We assume that the functions exported
     # by glib are the same for the native and cross builds
-    export GI_CROSS_LAUNCHER=$PREFIX/libexec/gi-cross-launcher-save.sh
-    make -j${CPU_COUNT}
-    make install
-    rm -rf $PREFIX/bin/g-ir-scanner $PREFIX/bin/g-ir-compiler
-    ln -s $BUILD_PREFIX/bin/g-ir-scanner $PREFIX/bin/g-ir-scanner
-    ln -s $BUILD_PREFIX/bin/g-ir-compiler $PREFIX/bin/g-ir-compiler
-    rsync -ahvpiI $BUILD_PREFIX/lib/gobject-introspection/ $PREFIX/lib/gobject-introspection/
+    export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-save.sh
+    ninja -j$CPU_COUNT -v
+    ninja install
     popd
   )
-  export GI_CROSS_LAUNCHER=$PREFIX/libexec/gi-cross-launcher-load.sh
+  export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-load.sh
 fi
 
-# Cf. https://github.com/conda-forge/staged-recipes/issues/673, we're in the
-# process of excising Libtool files from our packages. Existing ones can break
-# the build while this happens.
-find $PREFIX -name '*.la' -delete
+mkdir forgebuild
+cd forgebuild
 
-./configure --prefix=$PREFIX \
-            --with-xft \
-            --with-cairo=$PREFIX
-cat config.log
+export XDG_DATA_DIRS=${XDG_DATA_DIRS}:$PREFIX/share
+export PKG_CONFIG="$BUILD_PREFIX/bin/pkg-config"
+export PKG_CONFIG_PATH_FOR_BUILD="$BUILD_PREFIX/lib/pkgconfig"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig"
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != 1 ]]; then
+  export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig"
+fi
 
-make V=1
-# # FIXME: There is one failure:
-# ========================================
-#    pango 1.40.1: tests/test-suite.log
-# ========================================
-#
-# # TOTAL: 12
-# # PASS:  11
-# # SKIP:  0
-# # XFAIL: 0
-# # FAIL:  1
-# # XPASS: 0
-# # ERROR: 0
-#
-# .. contents:: :depth: 2
-#
-# FAIL: test-layout
-# =================
-#
-# /layout/valid-1.markup:
-# (/opt/conda/conda-bld/work/pango-1.40.1/tests/.libs/lt-test-layout:5078): Pango-CRITICAL **: pango_font_describe: assertion 'font != NULL' failed
-# FAIL test-layout (exit status: 133)
-# make check
-make install
-rm -rf $PREFIX/lib/gobject-introspection
+meson "${meson_options_host[@]}" $MESON_ARGS --prefix=$PREFIX ..
+ninja -j$CPU_COUNT -v
+ninja install
+
+cd $PREFIX
+rm -rf share/gtk-doc
+
